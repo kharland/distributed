c65e0f4232fad384bbfb70b2573401471210e500
Kendal Harland <kendaljharland@gmail.com>
2016-11-08 08:31:51 -0800

    Add coveralls support

---
+++ test/test_all.dart
@@ -0,0 +1,9 @@
+import 'data_channel_test.dart' as data_channel_test;
+import 'io_node_test.dart' as io_node_test;
+import 'message_handlers_test.dart' as message_handlers_test;
+
+void main() {
+  data_channel_test.main();
+  io_node_test.main();
+  message_handlers_test.main();
+}
