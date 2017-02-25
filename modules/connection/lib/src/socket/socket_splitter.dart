096c293cd518e41f1611256904cd547e8edf0f76
Kendal Harland <kharland@users.noreply.github.com>
2017-02-25 14:44:53 -0800

    Add docs to SocketSplitter (#27)
    * Add docs to SocketSplitter
    
    * Move logging into node
    
    * Update pubspec and presubmit
    
    * Add untracked log file

--- modules/connection/lib/src/socket/socket_splitter.dart
+++ modules/connection/lib/src/socket/socket_splitter.dart
@@ -4,13 +4,58 @@ import 'package:distributed.connection/src/socket/socket.dart';
 import 'package:stream_channel/stream_channel.dart';
 import 'package:test/src/utils.dart';
 
-abstract class SocketSplitter {
-  factory SocketSplitter(Socket socket) =>
-      new _SocketSplitter(socket, new _IntIdBroker());
+/// Splits a [Socket] into multiple [StreamChannel] instances.
+///
+/// Each [StreamChannel] is associated with an id which can be used to send
+/// message to another [StreamChannel] with the same id, which was also created
+/// by a [SocketSplitter].
+///
+/// It is not recommended to continue communicating directly over the socket
+/// once it has been split, as messages from all channels are sent over the
+/// socket with metadata. This metadata is used to determine which channel
+/// should receive a message.
+class SocketSplitter {
+  final Socket _socket;
+  final _MessageBroker _broker;
 
-  StreamChannel<String> get primaryChannel;
+  StreamChannel<String> _primaryChannel;
+  int _childId = 0;
 
-  Pair<int, StreamChannel<String>> split([int id]);
+  SocketSplitter(this._socket) : _broker = new _IntIdBroker() {
+    _primaryChannel = split(0).last;
+    _broker.addRecipient(_childId++, _primaryChannel.sink);
+    _socket.forEach(_broker.deliver);
+  }
+
+  /// The channel created when this splitter was constructed.
+  ///
+  /// This channel should be used instead of the original socket to send data,
+  /// such as a mapping of split [StreamChannel]s and their ids, to the client
+  /// at the other end of the socket.
+  StreamChannel<String> get primaryChannel => _primaryChannel;
+
+  /// Creates a new [StreamChannel] communicating over the original socket.
+  ///
+  /// The first element of the returned [Pair] is the channel's id, the second
+  /// element is the channel.
+  Pair<int, StreamChannel<String>> split([int id]) {
+    var controller = new StreamChannelController<String>(sync: true);
+    var channelId = id;
+    if (channelId == null) {
+      channelId = _childId++;
+    }
+
+    if (id != null && id >= _childId) {
+      _childId = id + 1;
+    }
+
+    _broker.addRecipient(channelId, controller.foreign.sink);
+    controller.foreign.stream.forEach((String message) {
+      _socket.add(_broker.addressMessage(channelId, message));
+    });
+
+    return new Pair<int, StreamChannel<String>>(channelId, controller.local);
+  }
 }
 
 abstract class _MessageBroker<T> {
@@ -50,42 +95,5 @@ class _IntIdBroker implements _MessageBroker<int> {
     message = parts.last;
     assert(_recipients.containsKey(recipientId));
     _recipients[recipientId].add(message);
-  }
-}
-
-class _SocketSplitter implements SocketSplitter {
-  final Socket _socket;
-  final _MessageBroker _broker;
-  StreamChannel<String> _primaryChannel;
-
-  int childId = 0;
-
-  _SocketSplitter(this._socket, this._broker) {
-    _primaryChannel = split(0).last;
-    _broker.addRecipient(childId++, _primaryChannel.sink);
-    _socket.forEach(_broker.deliver);
-  }
-
-  @override
-  StreamChannel<String> get primaryChannel => _primaryChannel;
-
-  @override
-  Pair<int, StreamChannel<String>> split([int id]) {
-    var controller = new StreamChannelController<String>(sync: true);
-    var channelId = id;
-    if (channelId == null) {
-      channelId = childId++;
-    }
-
-    if (id != null && id >= childId) {
-      childId = id + 1;
-    }
-
-    _broker.addRecipient(channelId, controller.foreign.sink);
-    controller.foreign.stream.forEach((String message) {
-      _socket.add(_broker.addressMessage(channelId, message));
-    });
-
-    return new Pair<int, StreamChannel<String>>(channelId, controller.local);
   }
 }
