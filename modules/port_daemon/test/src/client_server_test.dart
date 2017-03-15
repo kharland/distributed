2f6e0856c1c003980b3f7a2acdb1c683d7da18fc
kharland <kendaljharland@gmail.com>
2017-03-14 21:39:53 -0700

    Uncomment tests

--- modules/port_daemon/test/src/client_server_test.dart
+++ modules/port_daemon/test/src/client_server_test.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:distributed.objects/objects.dart';
 import 'package:distributed.port_daemon/port_daemon.dart';
 import 'package:distributed.port_daemon/port_daemon_client.dart';
+import 'package:distributed.port_daemon/ports.dart';
 import 'package:seltzer/platform/vm.dart';
 import 'package:test/test.dart';
 
@@ -39,49 +40,49 @@ void main() {
     });
   });
 
-//  group('$PortDaemon', () {
-//    setUp(() => commonSetUp());
-//
-//    tearDown(() => commonTearDown());
-//
-//    test('nodes should return the nodes registered with the daemon', () async {
-//      expect(daemon.nodes, isEmpty);
-//      await clientA.register();
-//      await clientB.register();
-//      expect(daemon.nodes, ['A', 'B']);
-//    });
-//
-//    test('should register a node', () async {
-//      expect(await clientA.register(), greaterThan(0));
-//      expect(await daemon.getPort('A'), greaterThan(0));
-//    });
-//
-//    test('should fail to register a currently registered node', () async {
-//      expect(await clientA.register(), greaterThan(0));
-//      expect(await clientA.register(), Ports.error);
-//    });
-//
-//    test('should be able to deregister a node', () async {
-//      expect(await clientA.register(), greaterThan(0));
-//      expect(await daemon.getPort('A'), greaterThan(0));
-//      expect(await clientA.deregister(), true);
-//      expect(await daemon.getPort('A'), Ports.error);
-//    });
-//
-//    test('should exchange the list of nodes registered on the server',
-//        () async {
-//      expect(await clientA.getNodes(), isEmpty);
-//      expect(await clientA.register(), greaterThan(0));
-//      expect(await clientB.register(), greaterThan(0));
-//      expect((await clientA.getNodes()).keys, unorderedEquals(['A', 'B']));
-//    });
-//
-//    test("should exchange a node's information", () async {
-//      expect(await clientA.lookup('A'), lessThan(0));
-//      var port = await clientA.register();
-//      expect(port, greaterThan(0));
-//      expect(await daemon.getPort('A'), port);
-//      expect(await clientA.lookup('A'), port);
-//    });
-//  });
+  group('$PortDaemon', () {
+    setUp(() => commonSetUp());
+
+    tearDown(() => commonTearDown());
+
+    test('nodes should return the nodes registered with the daemon', () async {
+      expect(daemon.nodes, isEmpty);
+      await clientA.register();
+      await clientB.register();
+      expect(daemon.nodes, ['A', 'B']);
+    });
+
+    test('should register a node', () async {
+      expect(await clientA.register(), greaterThan(0));
+      expect(await daemon.getPort('A'), greaterThan(0));
+    });
+
+    test('should fail to register a currently registered node', () async {
+      expect(await clientA.register(), greaterThan(0));
+      expect(await clientA.register(), Ports.error);
+    });
+
+    test('should be able to deregister a node', () async {
+      expect(await clientA.register(), greaterThan(0));
+      expect(await daemon.getPort('A'), greaterThan(0));
+      expect(await clientA.deregister(), true);
+      expect(await daemon.getPort('A'), Ports.error);
+    });
+
+    test('should exchange the list of nodes registered on the server',
+        () async {
+      expect(await clientA.getNodes(), isEmpty);
+      expect(await clientA.register(), greaterThan(0));
+      expect(await clientB.register(), greaterThan(0));
+      expect((await clientA.getNodes()).keys, unorderedEquals(['A', 'B']));
+    });
+
+    test("should exchange a node's information", () async {
+      expect(await clientA.lookup('A'), lessThan(0));
+      var port = await clientA.register();
+      expect(port, greaterThan(0));
+      expect(await daemon.getPort('A'), port);
+      expect(await clientA.lookup('A'), port);
+    });
+  });
 }
