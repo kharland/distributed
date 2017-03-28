c5196be56827bcb7f6dcef07be17d0da1f1b2718
kharland <kendaljharland@gmail.com>
2017-03-27 23:58:10 -0700

    Add router test to test_all.dart

--- test/test_all.dart
+++ test/test_all.dart
@@ -8,8 +8,10 @@ import 'src/signal_monitor_test.dart' as resource_test;
 import 'src/message_router_test.dart' as message_router_test;
 import 'src/vm_node_test.dart' as vm_node_test;
 import 'src/client_server_test.dart' as client_server_test;
+import 'src/http_server/router_test.dart' as router_test;
 
 void main() {
+  router_test.main();
   message_channel_test.main();
   message_router_test.main();
   periodic_function_test.main();
