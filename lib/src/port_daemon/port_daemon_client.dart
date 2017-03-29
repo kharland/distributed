2a92af99bd65bd0d09a2721d3357893cff9e773f
kharland <kendaljharland@gmail.com>
2017-03-29 00:14:33 -0700

    Add doc comments to new PortDaemonClient methods

--- lib/src/port_daemon/port_daemon_client.dart
+++ lib/src/port_daemon/port_daemon_client.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 
+import 'package:distributed/distributed.dart';
 import 'package:distributed/src/objects/interfaces.dart';
 import 'package:distributed/src/port_daemon/http_daemon_client.dart';
 import 'package:distributed/src/port_daemon/port_daemon.dart';
 import 'package:distributed/src/port_daemon/ports.dart';
 
-// TODO: Use HttpDaemonClient as collection of methods and refactor
-// This class into clients for web/node/cli/ etc.
 /// An object for communicating with a [PortDaemon].
 abstract class PortDaemonClient {
   factory PortDaemonClient(name, daemonHost) = HttpDaemonClient;
@@ -22,19 +21,28 @@ abstract class PortDaemonClient {
   /// Returns an empty map if no nodes are registered or if an error occurred.
   Future<Map<String, int>> getNodes();
 
-  /// Request the url for connecting to the node named [nodeName].
+  /// Returns the url for connecting to the node named [nodeName].
   ///
   /// Returns the empty string if [nodeName] could not be found.
   Future<String> lookup(String nodeName);
 
-  /// Instructs the daemon server to register this client under a new port.
+  /// Returns a new port for this client's owner [Node].
   ///
   /// Returns a Future that completes with the new port if registration
   /// succeeded or [Ports.error] if it failed.
   Future<int> registerNode();
 
+  /// Returns the url for the remote interaction server for the node named
+  /// [nodeName].
+  ///
+  /// Returns the empty string if no server was found.
   Future<String> lookupServer(String nodeName);
 
+  /// Returns a new port for this client's owner [Node] remote interaction
+  /// server.
+  ///
+  /// Returns a Future that completes with the new port if registration
+  /// succeeded or [Ports.error] if it failed.
   Future<int> registerServer();
 
   /// Instructs the daemon server to deregister this client.
