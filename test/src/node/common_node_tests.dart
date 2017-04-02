d6546b61e8b0569c44ed86a8667855cc586b4dec
kharland <kendaljharland@gmail.com>
2017-04-01 18:55:27 -0700

    uncomment tests

--- test/src/node/common_node_tests.dart
+++ test/src/node/common_node_tests.dart
@@ -33,50 +33,50 @@ void runNodeTests() {
       await ping.connect(pong.toPeer());
     });
 
-//    test('should register when a disconnection occurs', () async {
-//      await ping.connect(pong.toPeer());
-//      expect(pong.onDisconnect, emits(ping.toPeer()));
-//      expect(ping.onDisconnect, emits(pong.toPeer()));
-//      ping.disconnect(pong.toPeer());
-//    });
-//
-//    test('should update its list of peers when a connection is made', () async {
-//      await ping.connect(pong.toPeer());
-//      expect(ping.peers, [pong.toPeer()]);
-//      expect(pong.peers, [ping.toPeer()]);
-//    });
-//
-//    test('should update its list of peers when a disconnection is made',
-//        () async {
-//      await ping.connect(pong.toPeer());
-//      ping.disconnect(pong.toPeer());
-//      await ping.onDisconnect.first;
-//      expect(ping.peers, isEmpty);
-//      await pong.onDisconnect.first;
-//      expect(pong.peers, isEmpty);
-//    });
-//
-//    test('should send and receive messages', () async {
-//      await ping.connect(pong.toPeer());
-//
-//      pong.receive('ping').listen(expectAsync1((Message message) {
-//        expect(message, $message('ping', 'ping-message', ping.toPeer()));
-//        pong.send(ping.toPeer(), 'pong', 'pong-message');
-//      }));
-//      ping.receive('pong').listen(expectAsync1((Message message) {
-//        expect(message, $message('pong', 'pong-message', pong.toPeer()));
-//      }));
-//      ping.send(pong.toPeer(), 'ping', 'ping-message');
-//    });
-//
-//    test('should register when a node has disconnected', () async {
-//      await ping.connect(pong.toPeer());
-//      expect(ping.onDisconnect, emits(pong.toPeer()));
-//      expect(pong.onDisconnect, emits(ping.toPeer()));
-//      ping.disconnect(pong.toPeer());
-//      await pong.onDisconnect.first;
-//      expect(ping.peers, isEmpty);
-//      expect(pong.peers, isEmpty);
-//    });
+    test('should register when a disconnection occurs', () async {
+      await ping.connect(pong.toPeer());
+      expect(pong.onDisconnect, emits(ping.toPeer()));
+      expect(ping.onDisconnect, emits(pong.toPeer()));
+      ping.disconnect(pong.toPeer());
+    });
+
+    test('should update its list of peers when a connection is made', () async {
+      await ping.connect(pong.toPeer());
+      expect(ping.peers, [pong.toPeer()]);
+      expect(pong.peers, [ping.toPeer()]);
+    });
+
+    test('should update its list of peers when a disconnection is made',
+        () async {
+      await ping.connect(pong.toPeer());
+      ping.disconnect(pong.toPeer());
+      await ping.onDisconnect.first;
+      expect(ping.peers, isEmpty);
+      await pong.onDisconnect.first;
+      expect(pong.peers, isEmpty);
+    });
+
+    test('should send and receive messages', () async {
+      await ping.connect(pong.toPeer());
+
+      pong.receive('ping').listen(expectAsync1((Message message) {
+        expect(message, $message('ping', 'ping-message', ping.toPeer()));
+        pong.send(ping.toPeer(), 'pong', 'pong-message');
+      }));
+      ping.receive('pong').listen(expectAsync1((Message message) {
+        expect(message, $message('pong', 'pong-message', pong.toPeer()));
+      }));
+      ping.send(pong.toPeer(), 'ping', 'ping-message');
+    });
+
+    test('should register when a node has disconnected', () async {
+      await ping.connect(pong.toPeer());
+      expect(ping.onDisconnect, emits(pong.toPeer()));
+      expect(pong.onDisconnect, emits(ping.toPeer()));
+      ping.disconnect(pong.toPeer());
+      await pong.onDisconnect.first;
+      expect(ping.peers, isEmpty);
+      expect(pong.peers, isEmpty);
+    });
   });
 }
