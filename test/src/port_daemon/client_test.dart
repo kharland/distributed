447fdb7a67050fd98127092af4e9d5a49019120b
kharland <kendaljharland@gmail.com>
2017-04-09 21:13:38 -0700

    Delete unused test code

--- test/src/port_daemon/client_test.dart
+++ test/src/port_daemon/client_test.dart
@@ -53,8 +53,8 @@ void main() {
       });
 
       test(
-          "should return the empty string if the daemon doesn't return a response",
-          () async {
+          "should return the empty string if the daemon doesn't return a "
+          "response", () async {
         when(mockHttp.get(routes.node('a')))
             .thenReturn(new Future.delayed(timeoutPlus100ms));
         expect(getNodeConnectionUrl(http, routes, 'a'), completion(isEmpty));
@@ -71,8 +71,8 @@ void main() {
       });
 
       test(
-          "should return the empty string if the daemon doesn't return a response",
-          () async {
+          "should return the empty string if the daemon doesn't return a "
+          "response", () async {
         when(mockHttp.get(routes.controlServer('a')))
             .thenReturn(new Future.delayed(timeoutPlus100ms));
         expect(getNodeControlUrl(http, routes, 'a'), completion(isEmpty));
@@ -89,60 +89,14 @@ void main() {
       });
 
       test(
-          "should return the empty string if the daemon doesn't return a response",
-          () async {
+          "should return the empty string if the daemon doesn't return a "
+          "response", () async {
         when(mockHttp.get(routes.diagnosticsServer('a')))
             .thenReturn(new Future.delayed(timeoutPlus100ms));
         expect(getNodeDiagnosticsUrl(http, routes, 'a'), completion(isEmpty));
       });
     });
   });
-
-//  group('$PortDaemon', () {
-//    setUp(() => commonSetUp());
-//
-//    tearDown(() => commonTearDown());
-//
-//    test('should register a node', () async {
-//      expect(await clientA.registerNode(), greaterThan(0));
-//      expect(await daemon.getPort('A'), greaterThan(0));
-//    });
-//
-//    test('nodes should return the nodes registered with the daemon', () async {
-//      expect(daemon.nodes, isEmpty);
-//      await clientA.registerNode();
-//      await clientB.registerNode();
-//      expect(daemon.nodes, ['A', 'B']);
-//    });
-//
-//    test('should fail to register a currently registered node', () async {
-//      expect(await clientA.registerNode(), greaterThan(0));
-//      expect(await clientA.registerNode(), Ports.error);
-//    });
-//
-//    test('should be able to deregister a node', () async {
-//      expect(await clientA.registerNode(), greaterThan(0));
-//      expect(await daemon.getPort('A'), greaterThan(0));
-//      expect(await clientA.deregister(), true);
-//      expect(await daemon.getPort('A'), Ports.error);
-//    });
-//
-//    test('should exchange the list of nodes registered on the server',
-//        () async {
-//      expect(await clientA.getNodes(), isEmpty);
-//      expect(await clientA.registerNode(), greaterThan(0));
-//      expect(await clientB.registerNode(), greaterThan(0));
-//      expect((await clientA.getNodes()).keys, unorderedEquals(['A', 'B']));
-//    });
-//
-//    test("should exchange a node's information", () async {
-//      expect(await clientA.lookup('A'), '');
-//      var port = await clientA.registerNode();
-//      expect(port, greaterThan(0));
-//      expect(await daemon.getPort('A'), port);
-//      expect(await clientA.lookup('A'), 'ws://localhost:$port');
-//    });
-//  });
 }
 
 class MockHttp extends Mock implements Http {}
